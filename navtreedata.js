9887c575f7532c86b188007dc608db2355c9a302
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2024-11-22 17:17:48 +0000

    Deploying to docs from @ ENDERZOMBI102/sourcepp@206e5712448a694b057bbc1cef69fa9197cac709 🚀

---
+++ navtreedata.js
@@ -0,0 +1,93 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "SourcePP", "index.html", [
+    [ "Cache Generation Steps", "dc/d45/md_src_steampp_cache_README.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Included Libraries", "index.html#autotoc_md0", null ],
+      [ "Wrappers", "index.html#autotoc_md1", null ],
+      [ "Special Thanks", "index.html#autotoc_md2", null ],
+      [ "Gallery", "index.html#autotoc_md3", null ],
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Concepts", "concepts.html", "concepts" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/de9/structvtfpp_1_1ImagePixel_1_1RGBA16161616F.html",
+"d1/dbd/structmdlpp_1_1Movement.html#ad546207a7be4fdc23688d90d5a7c6919",
+"d2/dd2/classvpkpp_1_1PackFile.html#a46893bdc6813e9d6a1f71ef0a1df4901",
+"d3/d80/structvtfpp_1_1Resource.html#a6e37adb4ee39a3e91c2ff84a9aad54a1",
+"d4/da2/classtoolpp_1_1FGDWriter_1_1EntityWriter.html#abc4d7124f769496d40a0f1744b7b5491",
+"d5/d60/bsppp_8h.html#a9cb52214dd23962b3ae112be2b95e2d0a9bd93fb90d99295d942dae95a82ddb39",
+"d5/d8e/Value_8h.html#af5d3dcc8a6e393d5fd5384c83692ab1ea81a86551f854e526976db5b2d04a705d",
+"d5/dd3/structmdlpp_1_1VTX_1_1VTX.html#a828363379c84fa6c96346762094d1862",
+"d6/da8/PackFileType_8h.html#aa85a67fde1caa75abb46425eaa54d926a4bced2938402aa3454b4d019e09b1e0a",
+"d8/dc3/structtoolpp_1_1FGD_1_1Entity.html#a3cfc90550f7088f895a1bac751665393",
+"d8/df2/classvtfpp_1_1VTF.html#aa203515ee52f1d82378a4bd97823a51aaef853a56dc477894527f7511e87fb3cd",
+"d9/db1/Binary_8h.html#a34a6d6858beecc2a4066c1524bc78397",
+"da/de7/classvpkpp_1_1ZIP.html#ad5e304f1af7dcea96d6bc3084d877ad8",
+"dd/d11/namespacebsppp.html#a9cb52214dd23962b3ae112be2b95e2d0a36bdca1ec030e93c10f9a70c79468e99",
+"de/d62/classkvpp_1_1KV1ElementBase.html#ab4074aece0e504aa53962c01e146aec7",
+"df/df2/LumpData_8h.html#abf6f8f330d082fdb8c9e62cf9e5ba58a"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
